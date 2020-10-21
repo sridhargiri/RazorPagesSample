e59372c985f84e47d71efc72e31bb2704283bdc8
Sridhar S <sridhargiri1@gmail.com>
2020-10-21 16:01:39 +0530

    abstract factory (creditcard example)

---
+++ ConsoleApp1/ConsoleApp1/FactoryMethodDesignPattern.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+    public class Platinum : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+    public class Titanium : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+    public class MoneyBack : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+    public abstract class CreditCardFactory
+    {
+        protected abstract CreditCard MakeProduct();
+        public CreditCard CreateProduct()
+        {
+            return this.MakeProduct();
+        }
+    }
+    public class MoneyBackFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new MoneyBack();
+            return product;
+        }
+    }
+    public class PlatinumFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Platinum();
+            return product;
+        }
+    }
+    public class TitaniumFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Titanium();
+            return product;
+        }
+    }
+    public class FactoryMethodDesignPattern
+    {
+        static void Main(string[] args)
+        {
+
+            CreditCard creditCard = new PlatinumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.WriteLine("--------------");
+            creditCard = new MoneyBackFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }
+}
