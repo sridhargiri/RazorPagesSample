0c692c89204c55ada105772ce5413e8afbd75edd
Sridhar S <sridhargiri1@gmail.com>
2020-10-25 20:35:48 +0530

    add extra

---
+++ ConsoleApp1/ConsoleApp1/MultitonExample.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+
+    public enum MultitonType
+    {
+        Zero,
+        One,
+        Two
+    }
+
+    public class Multiton
+    {
+        private static readonly Dictionary<MultitonType, Multiton> instances =
+            new Dictionary<MultitonType, Multiton>();
+
+        private MultitonType type;
+
+        private Multiton(MultitonType type)
+        {
+            this.type = type;
+        }
+
+        public static Multiton GetInstance(MultitonType type)
+        {
+            // Lazy init (not thread safe as written)
+            // Recommend using Double Check Locking if needing thread safety
+            if (!instances.TryGetValue(type, out var instance))
+            {
+                instance = new Multiton(type);
+
+                instances.Add(type, instance);
+            }
+
+            return instance;
+        }
+
+        public override string ToString()
+        {
+            return "My type is " + this.type;
+        }
+        static void ChangeReferenceType(Student std2)
+        {
+            std2.StudentName = "Steve";
+        }
+        // Sample usage
+        public static void Main()
+        {
+
+            Student std1 = new Student();
+            std1.StudentName = "Bill";
+            Console.WriteLine(std1.StudentName);
+
+            ChangeReferenceType(std1);
+
+            Console.WriteLine(std1.StudentName);
+            var m0 = GetInstance(MultitonType.Zero);
+            var m1 = Multiton.GetInstance(MultitonType.One);
+            var m2 = Multiton.GetInstance(MultitonType.Two);
+
+            Console.WriteLine(m0);
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            A v1 = new A();
+            v1.val = 10;
+            methodtoshowref(ref v1);
+            Console.WriteLine(v1.val);
+        }
+        public static void methodtoshowref(ref A obj)
+        {
+            obj = null;
+        }
+    }
+
+    public class A
+    {
+        public int val
+        {
+            get;
+            set;
+        }
+    }
+    internal class Student
+    {
+        public string StudentName { get; internal set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Studying.cs
@@ -0,0 +1,68 @@
+﻿/// <summary>
+/// Stores signatures of various important methods related to the site.
+/// </summary>
+public interface ISiteInterface
+{
+};
+
+/// <summary>
+/// Skeleton of the singleton that inherits the interface.
+/// </summary>
+/// <summary>
+/// Sample singleton object.
+/// </summary>
+public sealed class SiteStructure : ISiteInterface
+{
+    /// <summary>
+    /// This is an expensive resource.
+    /// We need to only store it in one place.
+    /// </summary>
+    object[] _data = new object[10];
+
+    /// <summary>
+    /// Allocate ourselves.
+    /// We have a private constructor, so no one else can.
+    /// </summary>
+    static readonly SiteStructure _instance = new SiteStructure();
+
+    /// <summary>
+    /// Access SiteStructure.Instance to get the singleton object.
+    /// Then call methods on that instance.
+    /// </summary>
+    public static SiteStructure Instance
+    {
+        get { return _instance; }
+    }
+
+    /// <summary>
+    /// This is a private constructor, meaning no outsiders have access.
+    /// </summary>
+    private SiteStructure()
+    {
+        // Initialize members here.
+    }
+}
+
+/// <summary>
+/// Here is an example class where we use a singleton with the interface.
+/// </summary>
+class TestClass
+{
+    /// <summary>
+    /// Sample.
+    /// </summary>
+    public TestClass()
+    {
+        // Send singleton object to any function that can take its interface.
+        SiteStructure site = SiteStructure.Instance;
+        CustomMethod(site);
+    }
+
+    /// <summary>
+    /// Receives a singleton that adheres to the ISiteInterface interface.
+    /// </summary>
+    private void CustomMethod(ISiteInterface interfaceObject)
+    {
+        // Use the singleton by its interface.
+    }
+}
