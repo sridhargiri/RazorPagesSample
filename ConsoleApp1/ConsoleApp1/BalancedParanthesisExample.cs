63338cb2ae30e8a1aa9b019200a4cb756459ef0e
Sridhar S <sridhargiri1@gmail.com>
2020-11-02 20:29:40 +0530

    Hackerrank -> special palindrome & Geeksforgeeks -> balanced paranthesis

---
+++ ConsoleApp1/ConsoleApp1/BalancedParanthesisExample.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class BalancedParan
+    {
+        public class stack
+        {
+            public int top = -1;
+            public char[] items = new char[100];
+
+            public void push(char x)
+            {
+                if (top == 99)
+                {
+                    Console.WriteLine("Stack full");
+                }
+                else
+                {
+                    items[++top] = x;
+                }
+            }
+
+            char pop()
+            {
+                if (top == -1)
+                {
+                    Console.WriteLine("Underflow error");
+                    return '\0';
+                }
+                else
+                {
+                    char element = items[top];
+                    top--;
+                    return element;
+                }
+            }
+
+            Boolean isEmpty()
+            {
+                return (top == -1) ? true : false;
+            }
+        }
+
+        // Returns true if character1 and character2 
+        // are matching left and right Parenthesis */ 
+        static Boolean isMatchingPair(char character1,
+                                      char character2)
+        {
+            if (character1 == '(' && character2 == ')')
+                return true;
+            else if (character1 == '{' && character2 == '}')
+                return true;
+            else if (character1 == '[' && character2 == ']')
+                return true;
+            else
+                return false;
+        }
+
+        // Return true if expression has balanced 
+        // Parenthesis  
+        static Boolean areParenthesisBalanced(char[] exp)
+        {
+            // Declare an empty character stack */ 
+            Stack<char> st = new Stack<char>();
+
+            // Traverse the given expression to 
+            //   check matching parenthesis 
+            for (int i = 0; i < exp.Length; i++)
+            {
+                // If the exp[i] is a starting 
+                // parenthesis then push it 
+                if (exp[i] == '{' || exp[i] == '('
+                    || exp[i] == '[')
+                    st.Push(exp[i]);
+
+                //  If exp[i] is an ending parenthesis 
+                //  then pop from stack and check if the 
+                //   popped parenthesis is a matching pair 
+                if (exp[i] == '}' || exp[i] == ')'
+                    || exp[i] == ']')
+                {
+
+                    // If we see an ending parenthesis without 
+                    //   a pair then return false 
+                    if (st.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    // Pop the top element from stack, if 
+                    // it is not a pair parenthesis of 
+                    // character then there is a mismatch. This 
+                    // happens for expressions like {(}) 
+                    else if (!isMatchingPair(st.Pop(),
+                                             exp[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // If there is something left in expression 
+            // then there is a starting parenthesis without 
+            // a closing parenthesis  
+
+            if (st.Count == 0)
+                return true; // balanced 
+            else
+            { // not balanced 
+                return false;
+            }
+        }
+
+
+        // Driver code 
+        public static void Main(String[] args)
+        {
+            char[] exp = { '{', '(', ')', '}', '[', ']' };
+
+            // Function call 
+            if (areParenthesisBalanced(exp))
+                Console.WriteLine("Balanced ");
+            else
+                Console.WriteLine("Not Balanced ");
+        }
+    }
+
+    // This code is contributed by 29AjayKumar
+}
---
+++ ConsoleApp1/ConsoleApp1/SpecialPalindrome.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    // C# program to count special 
+    // Palindromic substring 
+    using System;
+
+    public class SpecialPalindrome
+    {
+
+        // Function to count special 
+        // Palindromic susbstring 
+        public static int CountSpecialPalindrome(String str)
+        {
+            int n = str.Length;
+
+            // store count of special 
+            // Palindromic substring 
+            int result = 0;
+
+            // it will store the count  
+            // of continues same char 
+            int[] sameChar = new int[n];
+            for (int v = 0; v < n; v++)
+                sameChar[v] = 0;
+
+            int i = 0;
+
+            // traverse string character 
+            // from left to right 
+            while (i < n)
+            {
+
+                // store same character count 
+                int sameCharCount = 1;
+
+                int j = i + 1;
+
+                // count smiler character 
+                while (j < n &&
+                       str[i] == str[j])
+                {
+                    sameCharCount++;
+                    j++;
+                }
+
+                // Case : 1 
+                // so total number of  
+                // substring that we can 
+                // generate are : K *( K + 1 ) / 2 
+                // here K is sameCharCount 
+                result += (sameCharCount *
+                          (sameCharCount + 1) / 2);
+
+                // store current same char  
+                // count in sameChar[] array 
+                sameChar[i] = sameCharCount;
+
+                // increment i 
+                i = j;
+            }
+
+            // Case 2: Count all odd length 
+            //         Special Palindromic 
+            //         substring 
+            for (int j = 1; j < n; j++)
+            {
+                // if current character is  
+                // equal to previous one  
+                // then we assign Previous  
+                // same character count to 
+                // current one 
+                if (str[j] == str[j - 1])
+                    sameChar[j] = sameChar[j - 1];
+
+                // case 2: odd length 
+                if (j > 0 && j < (n - 1) &&
+                   (str[j - 1] == str[j + 1] &&
+                    str[j] != str[j - 1]))
+                    result += Math.Min(sameChar[j - 1],
+                                      sameChar[j + 1]);
+            }
+
+            // subtract all single  
+            // length substring 
+            return result - n;
+        }
+
+        // Driver code 
+        public static void Main()
+        {
+            String str = "abccba";
+            Console.Write(CountSpecialPalindrome(str));
+        }
+    }
+
+    // This code is contributed by mits.
+}
