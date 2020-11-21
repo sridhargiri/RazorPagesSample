4e8033bf71e3e412a8f4fe2b25e573427a3b569c
sridhar s <sridhargiri1@gmail.com>
2020-11-21 13:22:00 +0530

    Athena & siemens health

--- ConsoleApp1/ConsoleApp1/AthenaHackerrank.cs
+++ ConsoleApp1/ConsoleApp1/AthenaHackerrank.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp1
@@ -26,9 +28,31 @@ namespace ConsoleApp1
             return key.Substring(key.Length - shift) + key.Substring(0, key.Length - shift);
         }
     }
-    public class Base26Do
+    public class AthenaHackerrank
     {
-
+        /*
+         pick the most featured product. if there is a tie display all the equal rank products in alphabetical order
+        example : [orange,orange,blue,blue,black,white]
+        output : blue,orange
+         */
+        public static void featuredProduct(List<string> products)
+        {
+            var result = products.GroupBy(n => n)
+                    .Select(c => new { Key = c.Key, total = c.Count() }).OrderByDescending(x => x.total).GroupBy(x => x.total);
+            if (result != null && result.Count() >= 1)
+            {
+                foreach (var item in result.First().OrderBy(x => x.Key))
+                {
+                    Console.WriteLine(item.Key);
+                }
+                return;
+            }
+        }
+        /*
+         left rotate by n shifts then right rotate by m shifts
+        ex - geekforgeeks , left 2 , right 3
+        out - eeksforgeeksg
+         */
         public static string getShiftedString(string s, int leftShifts, int rightShifts)
         {
             string temp = s.leftRotateShift(leftShifts).rightRotateShift(rightShifts);
@@ -36,6 +60,14 @@ namespace ConsoleApp1
         }
         public static void Main(string[] args)
         {
+            List<string> products = new List<string>();
+            products.Add("grey");
+            products.Add("orange");
+            products.Add("blue");
+            products.Add("blue");
+            products.Add("black");
+            products.Add("white");
+            featuredProduct(products);
             //string o = "cdefgab".rightRotateShift(4);
             string o = getShiftedString("abcd", 1, 0);
             Console.WriteLine(o);
---
+++ ConsoleApp1/ConsoleApp1/SiemensHealth.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class SiemensHealth
+    {
+        /*
+         Split string skip at double quotes
+         */
+        public static IEnumerable<String> ParseText(String line, Char delimiter, Char textQualifier)
+        {
+
+            if (line == null)
+                yield break;
+
+            else
+            {
+                char prevChar = '\0';
+                Char nextChar = '\0';
+                Char currentChar = '\0';
+
+                Boolean inString = false;
+
+                StringBuilder token = new StringBuilder();
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    currentChar = line[i];
+
+                    if (i > 0)
+                        prevChar = line[i - 1];
+                    else
+                        prevChar = '\0';
+
+                    if (i + 1 < line.Length)
+                        nextChar = line[i + 1];
+                    else
+                        nextChar = '\0';
+
+                    if (currentChar == textQualifier && (prevChar == '\0' || prevChar == delimiter) && !inString)
+                    {
+                        inString = true;
+                        continue;
+                    }
+
+                    if (currentChar == textQualifier && (nextChar == '\0' || nextChar == delimiter) && inString)
+                    {
+                        inString = false;
+                        continue;
+                    }
+
+                    if (currentChar == delimiter && !inString)
+                    {
+                        yield return token.ToString();
+                        token = token.Remove(0, token.Length);
+                        continue;
+                    }
+
+                    token = token.Append(currentChar);
+
+                }
+
+                yield return token.ToString();
+
+            }
+        }
+        /*
+         Consider only 5kg and 1kg packs are allowed to fill up given weight
+        example - 6kg can be filled with 5 and 1 but 7kg cannot be obtained using these packs
+        print "YES" if possible else print "NO"
+         */
+        public static void PrintPackages(int m, int n, int w)
+        {
+            if (w - m == n)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            string input = "one \"two two\" three \"four four\" five six";
+            var parsedText = ParseText(input, ' ', '"');
+            foreach (var item in parsedText)
+            {
+                Console.WriteLine(item);
+            }
+            PrintPackages(5, 1, 6); PrintPackages(5, 1, 7);
+        }
+    }
+}
