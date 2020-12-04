b126f51734d5892d8309a527e4077abc7a53a83f
sridhar s <sridhargiri1@gmail.com>
2020-12-04 23:54:47 +0530

    Athenahealth hacherrank 3rd question(min cost construction)

--- ConsoleApp1/ConsoleApp1/AthenaHackerrank.cs
+++ ConsoleApp1/ConsoleApp1/AthenaHackerrank.cs
@@ -48,6 +48,54 @@ namespace ConsoleApp1
                 return;
             }
         }
+
+        /*
+         * A construction company is building a new neighbourhood. Each house will be built using one of three main materials [wood,brick,cement], but no side by side houses should be built using the same material. Given an n*3 array where values in column respresents the cost of materials and "n" represents the number of houses , determine the minimum cost to build the neighbourhood.
+
+Example 1:
+given input : [[1,2,3],[1,2,3],[3,3,1]] (values in column represent cost of materials)
+
+output : 4
+
+Explanation:
+choose material-1 of cost 1 in first house(row)
+choose material-2 of cost 2 in second house(row)
+choose material-3 of cost 1 in third house (row)
+total = 1+2+1 -> 4
+
+Example 2:
+
+input: [[1,10,20],[2,100,200]]
+
+output: 12
+
+Explanation:
+choose material-2 of cost 10 in first house(row)
+choose material-1 of cost 2 in second house(row)
+total = 10 + 2 -> 12
+
+         * Complete the 'minCost' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY cost as parameter.
+         */
+
+        public static int minCost(List<List<int>> cost)
+        {
+            int price = 0;
+
+            for (int i = 1; i < cost.Count; i++)
+            {
+                cost[i][0] += Math.Min(cost[i - 1][1], cost[i - 1][2]);
+                cost[i][1] += Math.Min(cost[i - 1][0], cost[i - 1][2]);
+                cost[i][2] += Math.Min(cost[i - 1][1], cost[i - 1][0]);
+            }
+
+            price = Math.Min(Math.Min(cost[cost.Count - 1][0], cost[cost.Count - 1][1]), cost[cost.Count - 1][2]);
+            return price;
+        }
+
+
         /*
          left rotate by n shifts then right rotate by m shifts
         ex - geekforgeeks , left 2 , right 3
