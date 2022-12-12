a0a18415d01e0b098c9571636da32b96b1b8d2f0
sridhar <sridhargiri1@gmail.com>
2022-12-12 23:54:52 +0530

    Knight tour

--- ConsoleApp1/ConsoleApp1/GeneralCoding/MinStepsKnight.cs
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/MinStepsKnight.cs
@@ -4,13 +4,14 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    class MinStepsKnight
+    public class MinStepsKnight
     {
         /*
 		 Minimum steps to reach target by a Knight | Set 1
 		Difficulty Level : Hard
 		 Last Updated : 23 Jun, 2020
-		Given a square chessboard of N x N size, the position of Knight and position of a target is given. We need to find out the minimum steps a Knight will take to reach the target position.
+		Given a square chessboard of N x N size, the position of Knight and position of a target is given. 
+        We need to find out the minimum steps a Knight will take to reach the target position.
 
 		Examples:
 
@@ -19,7 +20,7 @@ namespace ConsoleApp1
 		In above diagram Knight takes 3 step to reach 
 		from (4, 5) to (1, 1) (4, 5) -> (5, 3) -> (3, 2) 
 		-> (1, 1)  as shown in diagram
-		Recommended: Please solve it on “PRACTICE ” first, before moving on to the solution.
+		
 		Approach:
 		This problem can be seen as shortest path in unweighted graph. Therefore we use BFS to solve this problem. We try all 8 possible positions where a Knight can reach from its position. If reachable position is not already visited and is inside the board, we push this state into queue with distance 1 more than its parent state. Finally we return distance of target position, when it gets pop out from queue.
 
@@ -115,17 +116,321 @@ namespace ConsoleApp1
                     knightPos,
                     targetPos, N));
         }
+        /*
+         Output: 20
+    Complexity Analysis:
+
+    Time complexity: O(N^2).
+    At worst case, all the cells will be visited so time complexity is O(N^2).
+    Auxiliary Space: O(N^2).
+    The nodes are stored in queue. So the space Complexity is O(N^2).
+        https://www.geeksforgeeks.org/minimum-steps-reach-target-knight/
+        image -> https://media.geeksforgeeks.org/wp-content/uploads/KnightChess.jpg
+        */
     }
     /*
-	 Output:
-20
-Complexity Analysis:
+     https://www.geeksforgeeks.org/possible-moves-knight/?ref=gcse
+    Possible moves of knight
+    Given a chess board of dimension m * n. Find number of possible moves where knight can be moved on a chessboard from given position. 
+    If mat[i][j] = 1 then the block is filled by something else, otherwise empty. 
+    Assume that board consist of all pieces of same color, i.e., there are no blocks being attacked
 
-Time complexity: O(N^2).
-At worst case, all the cells will be visited so time complexity is O(N^2).
-Auxiliary Space: O(N^2).
-The nodes are stored in queue. So the space Complexity is O(N^2).
-	https://www.geeksforgeeks.org/minimum-steps-reach-target-knight/
-	image -> https://media.geeksforgeeks.org/wp-content/uploads/KnightChess.jpg*/
+    Examples: 
+
+Input : mat[][] = {{1, 0, 1, 0},
+                   {0, 1, 1, 1},
+                   {1, 1, 0, 1},
+                   {0, 1, 1, 1}}
+        pos = (2, 2)
+Output : 4
+Knight can moved from (2, 2) to (0, 1), (0, 3), (1, 0) ans (3, 0).
+
+We can observe that knight on a chessboard moves either: 
+
+Two moves horizontal and one move vertical 
+Two moves vertical and one move horizontal
+
+The idea is to store all possible moves of knight and then count number of valid moves. A move will be invalid if:
+1.  A block is already occupied by another piece. 
+2.  Move is out of chessboard.
+
+Implementation:
+
+     */
+    public class PossibleMovesKnight
+    {
+        static int n = 4;
+        static int m = 4;
+
+        // To calculate possible moves
+        static int findPossibleMoves(int[,] mat, int p, int q)
+        {
+            /*
+            based on above observation,
+            1,2	
+            2,1	    reversed
+            1,-2
+            -2,1	reversed
+            -1,2
+            2,-1	reversed
+            -1,-2
+            -2,-1	reversed
+            
+            forming X and Y array as under
+             */
+            int[] X = { 2, 1, -1, -2, -2, -1, 1, 2 };
+            int[] Y = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+            int count = 0;
+
+            // Check if each possible
+            // move is valid or not
+            // iterate till 8, since 8 x 8 board
+            for (int i = 0; i < 8; i++)
+            {
+
+                // Position of knight
+                // after move
+                int x = p + X[i];
+                int y = q + Y[i];
+
+                // count valid moves
+                if (x >= 0 && y >= 0 && x < n && y < m && mat[x, y] == 0)
+                    count++;
+            }
+
+            // Return number
+            // of possible moves
+            return count;
+        }
+
+        // Driver Code
+        static public void Main()
+        {
+            int[,] mat = { { 1, 0, 1, 0 },
+                       { 0, 1, 1, 1 },
+                       { 1, 1, 0, 1 },
+                       { 0, 1, 1, 1 }};
+
+            int p = 2, q = 2;
+
+            Console.WriteLine(findPossibleMoves(mat, p, q));
+            /*
+            Output: 4
+            Time Complexity: O(1) 
+            Auxiliary Space: O(1)
+             */
+        }
+    }
+    /*
+     https://www.geeksforgeeks.org/the-knights-tour-problem-backtracking-1/?ref=gcse
+    The Knight’s tour problem | Backtracking-1
+Difficulty Level : Hard
+
+    Backtracking is an algorithmic paradigm that tries different solutions until finds a solution that “works”. Problems that are typically solved using the backtracking technique have the following property in common. These problems can only be solved by trying every possible configuration and each configuration is tried only once. A Naive solution for these problems is to try all configurations and output a configuration that follows given problem constraints. Backtracking works incrementally and is an optimization over the Naive solution where all possible configurations are generated and tried.
+For example, consider the following Knight’s Tour problem. 
+    http://en.wikipedia.org/wiki/Knight%27s_tour
+
+    Problem Statement:
+Given a N*N board with the Knight placed on the first block of an empty board. 
+    Moving according to the rules of chess knight must visit each square exactly once. 
+    Print the order of each cell in which they are visited.
+    Example:
+
+Input : 
+N = 8
+Output:
+0  59  38  33  30  17   8  63
+37  34  31  60   9  62  29  16
+58   1  36  39  32  27  18   7
+35  48  41  26  61  10  15  28
+42  57   2  49  40  23   6  19
+47  50  45  54  25  20  11  14
+56  43  52   3  22  13  24   5
+51  46  55  44  53   4  21  12
+The path followed by Knight to cover all the cells
+Following is a chessboard with 8 x 8 cells. Numbers in cells indicate the move number of Knight. 
+
+    Let us first discuss the Naive algorithm for this problem and then the Backtracking algorithm.
+
+Naive Algorithm for Knight’s tour 
+The Naive Algorithm is to generate all tours one by one and check if the generated tour satisfies the constraints. 
+
+while there are untried tours
+{ 
+   generate the next tour 
+   if this tour covers all squares 
+   { 
+      print this path;
+   }
+}
+
+Backtracking works in an incremental way to attack problems. 
+    Typically, we start from an empty solution vector and one by one add items (Meaning of item varies from problem to problem. In the context of Knight’s tour problem, an item is a Knight’s move). 
+    When we add an item, we check if adding the current item violates the problem constraint, if it does then we remove the item and try other alternatives. 
+    If none of the alternatives works out then we go to the previous stage and remove the item added in the previous stage. If we reach the initial stage back then we say that no solution exists. 
+    If adding an item doesn’t violate constraints then we recursively add items one by one. If the solution vector becomes complete then we print the solution
+
+    Backtracking Algorithm for Knight’s tour 
+
+Following is the Backtracking algorithm for Knight’s tour problem. 
+
+If all squares are visited 
+    print the solution
+Else
+   a) Add one of the next moves to solution vector and recursively 
+   check if this move leads to a solution. (A Knight can make maximum 
+   eight moves. We choose one of the 8 moves in this step).
+   b) If the move chosen in the above step doesn't lead to a solution
+   then remove this move from the solution vector and try other 
+   alternative moves.
+   c) If none of the alternatives work then return false (Returning false 
+   will remove the previously added item in recursion and if false is 
+   returned by the initial call of recursion then "no solution exists" )
+Following are implementations for Knight’s tour problem. It prints one of the possible solutions in 2D matrix form. Basically, the output is a 2D 8*8 matrix with numbers from 0 to 63 and these numbers show steps made by Knight
+
+
+     */
+    public class KnightsTourProblem
+    {
+        static int N = 8;
+
+        /* A utility function to
+        check if i,j are valid
+        indexes for N*N chessboard */
+        static bool isSafe(int x, int y, int[,] sol)
+        {
+            return (x >= 0 && x < N && y >= 0 && y < N
+                    && sol[x, y] == -1);
+        }
+
+        /* A utility function to
+        print solution matrix sol[N][N] */
+        static void printSolution(int[,] sol)
+        {
+            for (int x = 0; x < N; x++)
+            {
+                for (int y = 0; y < N; y++)
+                    Console.Write(sol[x, y] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        /* This function solves the
+        Knight Tour problem using
+        Backtracking. This function
+        mainly uses solveKTUtil() to
+        solve the problem. It returns
+        false if no complete tour is
+        possible, otherwise return true
+        and prints the tour. Please note
+        that there may be more than one
+        solutions, this function prints
+        one of the feasible solutions. */
+        static bool solveKT()
+        {
+            int[,] sol = new int[8, 8];
+
+            /* Initialization of
+            solution matrix */
+            for (int x = 0; x < N; x++)
+                for (int y = 0; y < N; y++)
+                    sol[x, y] = -1;
+
+            /* xMove[] and yMove[] define
+               next move of Knight.
+               xMove[] is for next
+               value of x coordinate
+               yMove[] is for next
+               value of y coordinate */
+            int[] xMove = { 2, 1, -1, -2, -2, -1, 1, 2 };
+            int[] yMove = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+            // Since the Knight is
+            // initially at the first block
+            sol[0, 0] = 0;
+
+            /* Start from 0,0 and explore
+            all tours using solveKTUtil() */
+            if (!solveKTUtil(0, 0, 1, sol, xMove, yMove))
+            {
+                Console.WriteLine("Solution does "
+                                  + "not exist");
+                return false;
+            }
+            else
+                printSolution(sol);
+
+            return true;
+        }
+
+        /* A recursive utility function
+        to solve Knight Tour problem */
+        static bool solveKTUtil(int x, int y, int movei,
+                                int[,] sol, int[] xMove,
+                                int[] yMove)
+        {
+            int k, next_x, next_y;
+            if (movei == N * N)
+                return true;
+
+            /* Try all next moves from
+            the current coordinate x, y */
+            for (k = 0; k < 8; k++)
+            {
+                next_x = x + xMove[k];
+                next_y = y + yMove[k];
+                if (isSafe(next_x, next_y, sol))
+                {
+                    sol[next_x, next_y] = movei;
+                    if (solveKTUtil(next_x, next_y, movei + 1,
+                                    sol, xMove, yMove))
+                        return true;
+                    else
+                        // backtracking
+                        sol[next_x, next_y] = -1;
+                }
+            }
+
+            return false;
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            // Function Call
+            solveKT();
+        }
+        /*
+         Output
+  0  59  38  33  30  17   8  63 
+ 37  34  31  60   9  62  29  16 
+ 58   1  36  39  32  27  18   7 
+ 35  48  41  26  61  10  15  28 
+ 42  57   2  49  40  23   6  19 
+ 47  50  45  54  25  20  11  14 
+ 56  43  52   3  22  13  24   5 
+ 51  46  55  44  53   4  21  12 
+Time Complexity : 
+There are N2 Cells and for each, we have a maximum of 8 possible moves to choose from, so the worst running time is O(8N^2).
+
+Auxiliary Space: O(N2)
+
+Important Note:
+No order of the xMove, yMove is wrong, but they will affect the running time of the algorithm drastically. 
+        For example, think of the case where the 8th choice of the move is the correct one, and before that our code ran 7 different wrong paths. 
+        It’s always a good idea a have a heuristic than to try backtracking randomly. Like, in this case, we know the next step would probably be in the south or east direction, then checking the paths which lead their first is a better strategy.
+
+Note that Backtracking is not the best solution for the Knight’s tour problem. See the below article for other better solutions. 
+        The purpose of this post is to explain Backtracking with an example. 
+        https://www.geeksforgeeks.org/warnsdorffs-algorithm-knights-tour-problem/
+
+Refer:
+http://see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf 
+http://www.cis.upenn.edu/~matuszek/cit594-2009/Lectures/35-backtracking.ppt 
+http://mathworld.wolfram.com/KnightsTour.html 
+http://en.wikipedia.org/wiki/Knight%27s_tour 
+         */
+    }
 
 }
---
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/TripletSumThreeArray.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /*
+    https://www.geeksforgeeks.org/find-three-element-from-given-three-arrays-such-that-their-sum-is-x-set-2/
+     Find three element from given three arrays such that their sum is X | Set 2
+    Given three sorted integer arrays A[], B[] and C[], the task is to find three integers, one from each array such that they sum up to a given target value X. Print Yes or No depending on whether such triplet exists or not.
+Examples: 
+ 
+
+Input: A[] = {2}, B[] = {1, 6, 7}, C[] = {4, 5}, X = 12 
+Output: Yes 
+A[0] + B[1] + C[0] = 2 + 6 + 4 = 12
+Input: A[] = {2}, B[] = {1, 6, 7}, C[] = {4, 5}, X = 14 
+Output: Yes 
+A[0] + B[2] + C[1] = 2 + 7 + 5 = 14 
+
+    Approach: We have already discusses a hash based approach in this article which takes O(N) extra space. 
+In this article, we will solve this problem using space efficient method that takes O(1) extra space. The idea is using two pointer technique. 
+We will iterate through the smallest of all the arrays and for each index i, we will use two-pointer on the larger two arrays to find a pair with sum equal to X – A[i] (assuming A[] is the smallest in length among the three arryas). 
+Now, what is the idea behind using two pointer on larger two arrays? We will try to understand the same from an example. 
+
+Let’s assume 
+len(A) = 100000 
+len(B) = 10000 
+len(C) = 10
+Case 1: Applying two pointer on larger two arrays 
+Number of iterations will be of order = len(C) * (len(A) + len(B)) = 10 * (110000) = 1100000
+Case 2: Applying two pointer on smaller two arrays 
+Number of iterations will be of order = len(A) * (len(B) + len(C)) = 100000 * (10010) = 1001000000
+Case 3: Applying two pointer on smallest and largest array 
+Number of iterations will be of order = len(B) * (len(A) + len(C)) = 10000 * (100000 + 10) = 1000100000
+As we can see, Case 1 is the most optimal for this example and it can be easily proved that its most optimal in general as well. 
+ 
+
+Algorithm: 
+ 
+
+Sort the arrays in increasing order of there lengths.
+Let’s say the smallest array after sorting is A[]. Then, iterate through all the elements of A[] and for each index ‘i’, apply two-pointer on the other two arrays. We will put a pointer on the beginning of array B[] and a pointer to end of array C[]. Let’s call the pointer ‘j’ and ‘k’ respectively. 
+If B[j] + C[k] = X – A[i], we found a match.
+If B[j] + C[k] less than X – A[i], we increase value of ‘j’ by 1.
+If B[j] + C[k] greater than X – A[i], we decrease value of ‘k’ by 1.
+Below is the implementation of the above approach: 
+     */
+    public class TripletSumThreeArray
+    {
+
+        // Function that returns true if there
+        // exists a triplet with sum x
+        static bool existsTriplet(int[] a, int[] b,
+                                  int[] c, int x, int l1,
+                                  int l2, int l3)
+        {
+            // Sorting arrays such that a[]
+            // represents smallest array
+            if (l2 <= l1 && l2 <= l3)
+            {
+                swap(l2, l1);
+                swap(a, b);
+            }
+            else if (l3 <= l1 && l3 <= l2)
+            {
+                swap(l3, l1);
+                swap(a, c);
+            }
+
+            // Iterating the smallest array
+            for (int i = 0; i < l1; i++)
+            {
+
+                // Two pointers on second and third array
+                int j = 0, k = l3 - 1;
+                while (j < l2 && k >= 0)
+                {
+
+                    // If a valid triplet is found
+                    if (a[i] + b[j] + c[k] == x)
+                        return true;
+                    if (a[i] + b[j] + c[k] < x)
+                        j++;
+                    else
+                        k--;
+                }
+            }
+            return false;
+        }
+
+        private static void swap(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        private static void swap(int[] x, int[] y)
+        {
+            int[] temp = x;
+            x = y;
+            y = temp;
+        }
+
+        // Driver code
+        public static void Main(String[] args)
+        {
+            int[] a = { 2, 7, 8, 10, 15 };
+            int[] b = { 1, 6, 7, 8 };
+            int[] c = { 4, 5, 5 };
+            int l1 = a.Length;
+            int l2 = b.Length;
+            int l3 = c.Length;
+
+            int x = 14;
+
+            if (existsTriplet(a, b, c, x, l1, l2, l3))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            /*
+             Output: Yes
+
+Time complexity: O(min(len(A), len(B), len(C)) * max(len(A), len(B), len(C)))
+Auxiliary Space: O(1),  since no extra space has been taken.
+            */
+        }
+    }
+}
