165a7993b6177ef3935a53c3c9e99d99460d5cc4
sridhar <sridhargiri1@gmail.com>
2023-02-07 23:47:37 +0530

    hackerrank poker nim

--- ConsoleApp1/ConsoleApp1/GeneralCoding/AppleOrange.cs
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/AppleOrange.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    class AppleOrange
+    public class AppleOrange
     {
         // Complete the countApplesAndOranges function below.
         // https://www.hackerrank.com/challenges/apple-and-orange/problem
@@ -60,6 +60,41 @@ namespace ConsoleApp1
             int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
             ;
             countApplesAndOranges(s, t, a, b, apples, oranges);
+        }
+    }
+
+    /*
+     In this HackerRank Poker Nim problem solution, we have given the values of N piles of chips indexed from 0 to n -1 and an integer K to ensure that the games end infinite time and the number of chips in each of the N piles, 
+    determine whether the person who wins the game is the first or second person to move. assume both players move optimally.
+    https://www.hackerrank.com/challenges/poker-nim-1/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=30-day-campaign
+    https://programs.programmingoneonone.com/2021/07/hackerrank-poker-nim-problem-solution.html
+     */
+    public class PokerNim
+    {
+        public static void Main(string[] args)
+        {
+
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                int[] n = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp));
+
+                int res = 0;
+                int[] _t = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp));
+
+                for (int i = 0; i < _t.Length; i++)
+                {
+                    res ^= _t[i];
+                }
+                if (res == 0)
+                {
+                    Console.WriteLine("Second");
+                }
+                else
+                {
+                    Console.WriteLine("First");
+                }
+            }
         }
     }
 }
