a58fea96c0f1038e15fc72a09f69219a4a0c6e5d
sridhar <sridhargiri1@gmail.com>
2023-02-28 22:22:10 +0530

    Aploris test part 1

--- ConsoleApp1/ConsoleApp1/GeneralCoding/Urlify.cs
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/Urlify.cs
@@ -108,4 +108,73 @@ Auxiliary Space: O(1), because the above program is an inplace algorithm.
             */
         }
     }
+    class Department
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int BuildingId { get; set; }
+    }
+    public class Ask2
+    {
+        static void Main(string[] args)
+        {
+            var idx = FirstIndexOfElement("2", new object[] { "1", "2", "3" ,"2"});
+            Console.WriteLine(idx);
+            var s=RecursiveStringReverse("sridhar");
+            Console.WriteLine(s);
+            var lst=ReverseList<int>(new List<int> {4,3,2,1 });
+            lst.ForEach((j) => Console.WriteLine(j));
+            Console.WriteLine(   GetBitStringForInt(12));
+            var list = new List<Department>()
+ {
+ new Department() { Name ="Finance",Number = 1,BuildingId = 15 },
+ new Department() { Name ="HR",Number = 2,BuildingId = 3 },
+ new Department() { Name ="IT",Number = 3,BuildingId = 15 },
+ new Department() { Name ="Sales", Number = 4, BuildingId = 3 }
+ };
+            var newList = from department in list
+                          group department by department.BuildingId into dp
+                          select new { counter = dp.Key, department = dp };
+            Console.Out.WriteLine(newList.Count());
+        }
+        public static int FirstIndexOfElement( object element, object[] array)
+        {
+            for (int i = 0; i < array.Length;)
+            {
+                if (array[++i] == element) return i;
+            }
+            return -1;
+        }
+
+        // Method returns new list with elements from input list in reversed order
+        public static List<T> ReverseList<T>(List<T> elems)
+        {
+            var newList = new List<T>();
+            for (int i = 1; i <= elems.Count; i++)
+            {
+                newList.Add(elems[elems.Count - i ]);
+            }
+            return newList;
+        }
+        public static string GetBitStringForInt(int n)
+        {
+            StringBuilder rtn = new StringBuilder();
+            for (int i = 31; i >= 0; i--)
+            {
+                int v = (1 << i) & n;
+                rtn.Append(v == 1 ? "1" : "0");
+            }
+            return rtn.ToString();
+        }
+
+        public static string RecursiveStringReverse(string input)
+        {
+            if (input.Length <= 1)
+                return input;
+            char firstChar = input[0];
+            string lastChars = input.Substring(1);
+            return RecursiveStringReverse(lastChars) + firstChar;
+
+        }
+    }
 }
