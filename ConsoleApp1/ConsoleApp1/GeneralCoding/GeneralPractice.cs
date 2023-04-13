b18d5b9f7af53d1a56fc50b5a1d45f79e744187a
sridhar <sridhargiri1@gmail.com>
2023-04-13 22:21:30 +0530

    Event example

--- ConsoleApp1/ConsoleApp1/GeneralCoding/GeneralPractice.cs
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/GeneralPractice.cs
@@ -118,4 +118,58 @@ namespace ConsoleApp1
         //output double array argument
         //bcos since both object & less derived type is there, null choose the less derived i.e. double
     }
+    public class ProcessEventArgs : EventArgs
+    {
+        public bool IsSuccessful { get; set; }
+        public DateTime CompletionTime { get; set; }
+    }
+    public class EventExample
+    {
+        public static void Main()
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+
+        // event handler
+        public static void bl_ProcessCompleted(object sender, ProcessEventArgs e)
+        {
+            Console.WriteLine("Process " + (e.IsSuccessful ? "Completed Successfully" : "failed"));
+            Console.WriteLine("Completion Time: " + e.CompletionTime.ToLongDateString());
+        }
+    }
+
+    public class ProcessBusinessLogic
+    {
+        // declaring an event using built-in EventHandler
+        public event EventHandler<ProcessEventArgs> ProcessCompleted;
+
+        public void StartProcess()
+        {
+            var data = new ProcessEventArgs();
+
+            try
+            {
+                Console.WriteLine("Process Started!");
+
+                // some code here..
+
+                data.IsSuccessful = true;
+                data.CompletionTime = DateTime.Now;
+                OnProcessCompleted(data);
+            }
+            catch (Exception ex)
+            {
+                data.IsSuccessful = false;
+                data.CompletionTime = DateTime.Now;
+                OnProcessCompleted(data);
+            }
+        }
+
+        protected virtual void OnProcessCompleted(ProcessEventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
 }
