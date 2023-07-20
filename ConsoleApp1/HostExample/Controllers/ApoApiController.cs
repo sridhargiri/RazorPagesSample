4e20a177f28d1dae039aa9dca66cc2590252f185
sridhar <sridhargiri1@gmail.com>
2023-07-20 21:44:32 +0530

    Shell july 2023

--- ConsoleApp1/HostExample/Controllers/ApoApiController.cs
+++ ConsoleApp1/HostExample/Controllers/ApoApiController.cs
@@ -77,6 +77,156 @@ namespace HostExample.Controllers
         }
     }
 
+    /*
+     Shell codility test taken at 20/07/2023 at 7.30pm failed
+     */
+
+    public interface IWarehouseRepository
+    {
+        void SetCapacityRecord(int productId, int capacity);
+        IEnumerable<CapacityRecord> GetCapacityRecords();
+        IEnumerable<CapacityRecord> GetCapacityRecords(Func<CapacityRecord, bool> filter);
+
+        void SetProductRecord(int productId, int quantity);
+        IEnumerable<ProductRecord> GetProductRecords();
+        IEnumerable<ProductRecord> GetProductRecords(Func<ProductRecord, bool> filter);
+    }
+    public class CapacityRecord
+    {
+        public int ProductId { get; set; }
+        public int Capacity { get; set; }
+    }
+
+    public class ProductRecord
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class WarehouseEntry
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    // NotPositiveQuantityMessage should be returned by
+    // SetProductCapacity, ReceiveProduct and DispatchProduct methods
+    public class NotPositiveQuantityMessage
+    {
+        public string Message => "A positive quantity was not provided";
+    }
+
+    // QuantityTooLowMessage should be returned by
+    // SetProductCapacity, ReceiveProduct and DispatchProduct methods
+    public class QuantityTooLowMessage
+    {
+        public string Message => "Too low a quantity was provided";
+    }
+
+    // QuantityTooHighMessage should be returned by
+    // SetProductCapacity, ReceiveProduct and DispatchProduct methods
+    public class QuantityTooHighMessage
+    {
+        public string Message => "Too high a quantity was provided";
+    }
+    public class WarehouseController : Controller
+    {
+        private readonly IWarehouseRepository _warehouseRepository;
+
+        public WarehouseController(IWarehouseRepository warehouseRepository)
+        {
+            _warehouseRepository = warehouseRepository;
+        }
+
+        // Return OkObjectResult(IEnumerable<WarehouseEntry>)
+        public IActionResult GetProducts()
+        {
+            // Console.WriteLine("Sample debug output");
+            var ss=_warehouseRepository.GetProductRecords().Where(t=>t.Quantity>0);
+            List<WarehouseEntry> wh = new List<WarehouseEntry>();
+            foreach (var item in ss)
+            {
+                WarehouseEntry w = new WarehouseEntry();
+                w.ProductId = item.ProductId;
+                w.Quantity = item.Quantity;
+                wh.Add(w);
+            }
+            return new OkObjectResult(wh);
+        }
+        /*
+         
+        */
+        // Return OkResult, BadRequestObjectResult(NotPositiveQuantityMessage), or BadRequestObjectResult(QuantityTooLowMessage)
+        public IActionResult SetProductCapacity(int productId, int capacity)
+        {
+            var prod = _warehouseRepository.GetProductRecords().FirstOrDefault(t => t.ProductId == productId);
+            var qty = prod?.Quantity;
+            if (capacity < qty)
+            {
+                QuantityTooLowMessage qt = new QuantityTooLowMessage();
+                return new BadRequestObjectResult(qt);
+            }
+            else if (capacity <= 0)
+            {
+               
+                NotPositiveQuantityMessage msg = new NotPositiveQuantityMessage();
+                return new BadRequestObjectResult(msg);
+            }
+            else
+            {
+                _warehouseRepository.SetCapacityRecord(productId, capacity);
+                return new OkResult();
+            }
+
+        }
+
+        // Return OkResult, BadRequestObjectResult(NotPositiveQuantityMessage), or BadRequestObjectResult(QuantityTooHighMessage)
+        public IActionResult ReceiveProduct(int productId, int qty)
+        {
+            var prod = _warehouseRepository.GetProductRecords().FirstOrDefault(t => t.ProductId == productId);
+            var qty1 = prod?.Quantity;
+            qty1 = qty1.HasValue ? qty1.Value : 0;
+            if (qty1+qty > qty1)
+            {
+                QuantityTooLowMessage qt = new QuantityTooLowMessage();
+                return new BadRequestObjectResult(qt);
+            }
+            else if (qty <= 0)
+            {
+                NotPositiveQuantityMessage msg = new NotPositiveQuantityMessage();
+                return new BadRequestObjectResult(msg);
+            }
+            else
+            {
+                _warehouseRepository.SetCapacityRecord(productId, qty);
+                return new OkResult();
+            }
+
+        }
+
+        // Return OkResult, BadRequestObjectResult(NotPositiveQuantityMessage), or BadRequestObjectResult(QuantityTooHighMessage)
+        public IActionResult DispatchProduct(int productId, int qty)
+        {
+            var prod = _warehouseRepository.GetProductRecords().FirstOrDefault(t => t.ProductId == productId);
+            var _qty = prod?.Quantity;
+            _qty = _qty.HasValue ? _qty.Value : 0;
+            if (qty < 0)
+            {
+                QuantityTooLowMessage qt = new QuantityTooLowMessage();
+                return new BadRequestObjectResult(qt);
+            }
+            else if (qty > _qty)
+            {
+                QuantityTooHighMessage msg = new QuantityTooHighMessage();
+                return new BadRequestObjectResult(msg);
+            }
+            else
+            {
+                prod.Quantity = prod.Quantity - qty;
+                return new OkResult();
+            }
+        }
+    }
+
     public interface IRepository
     {
         ICollection<Store> GetStores(Func<Store, bool> filter, bool includeCustomers = false);
