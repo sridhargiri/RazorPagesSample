9624a53445a209a6efbb3fec88938f8c08ff83bb
sridhar <sridhargiri1@gmail.com>
2024-02-20 00:09:34 +0530

    hcl appscan

---
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/AngleHourMinute.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AngleHourMinute
+    {
+        /*
+            hcl appscan speedexam online test attended 16/02/2024
+        https://www.geeksforgeeks.org/calculate-angle-hour-hand-minute-hand/
+        Calculate the angle between hour hand and minute hand
+        This problem is known as Clock angle problem where we need to find angle between hands of an analog clock at a given time.
+Examples: 
+
+Input:  
+h = 12:00
+m = 30.00
+Output: 
+165 degree
+
+Input:  
+h = 3.00
+m = 30.00
+Output: 
+75 degree
+The idea is to take 12:00 (h = 12, m = 0) as a reference. Following are detailed steps.
+
+1. Calculate the angle made by hour hand with respect to 12:00 in h hours and m minutes. 
+2. Calculate the angle made by minute hand with respect to 12:00 in h hours and m minutes. 
+3. The difference between the two angles is the angle between the two hands.
+
+How to calculate the two angles with respect to 12:00? 
+The minute hand moves 360 degrees in 60 minute(or 6 degrees in one minute) and hour hand moves 360 degrees in 12 hours(or 0.5 degrees in 1 minute). 
+In h hours and m minutes, the minute hand would move (h*60 + m)*6 and hour hand would move (h*60 + m)*0.5.
+         */
+        static int CalculateAngle(double h, double m)
+        {
+            // validate the input
+            if (h < 0 || m < 0 ||
+                h > 12 || m > 60)
+                Console.Write("Wrong input");
+
+            if (h == 12)
+                h = 0;
+
+            if (m == 60)
+            {
+                m = 0;
+                h += 1;
+                if (h > 12)
+                    h = h - 12;
+            }
+
+            // Calculate the angles moved by hour and
+            // minute hands with reference to 12:00
+            int hour_angle = (int)(0.5 * (h * 60 + m));
+            int minute_angle = (int)(6 * m);
+
+            // Find the difference between two angles
+            int angle = Math.Abs(hour_angle - minute_angle);
+
+            // smaller angle of two possible angles
+            angle = Math.Min(360 - angle, angle);
+
+            return angle;
+        }
+
+        // Driver code
+        public static void Main()
+        {
+            Console.WriteLine(CalculateAngle(10, 0));
+            Console.Write(CalculateAngle(3, 30));
+            /*
+             Output 60 75
+Time Complexity: O(1)
+Auxiliary Space: O(1)
+            */
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/ConsecutiveDigitSum.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /*
+    https://www.geeksforgeeks.org/sum-of-numbers-formed-by-consecutive-digits-present-in-a-given-string/
+    hcl appscan speedexam online test attended 16/02/2024
+    Sum of numbers formed by consecutive digits present in a given string
+    Given a string S consisting of digits [0 – 9] and lowercase alphabets, the task is to calculate the sum of all numbers represented by continuous sequences of digits present in the string S.
+
+Examples:
+
+Input: S = “11aa32bbb5”
+Output: 48
+Explanation: 
+The consecutive sequence of numbers present in the string S are {11, 32, 5}.
+Therefore, sum = 11 + 32 + 5 = 48
+
+Input: s = “5an63ff2”
+Output: 70
+
+Approach: Follow the steps below to solve the problem:
+
+Initialize a variable, say curr, to store the current sequence of consecutive digits
+Iterate over the characters of the string.
+If the current character is not a digit, add the current value of curr to the final answer. Reset curr to 0.
+Otherwise, append the current digit to curr.
+Finally, return the final answer.
+Below is the implementation of the above approach:
+     */
+    public class ConsecutiveDigitSum
+    {
+
+        // Function to calculate the sum of 
+        // numbers formed by consecutive 
+        // sequences of digits present in the string 
+        static int sumOfDigits(string s)
+        {
+
+            // Stores consecutive digits 
+            // present in the string 
+            int curr = 0;
+
+            // Stores the sum 
+            int ret = 0;
+
+            // Iterate over characters 
+            // of the input string 
+            foreach (char ch in s)
+            {
+
+                // If current character is a digit 
+                if (ch >= 48 && ch <= 57)
+                {
+
+                    // Append current digit to curr 
+                    curr = curr * 10 + ch - '0';
+                }
+                else
+                {
+
+                    // Add curr to sum 
+                    ret += curr;
+
+                    // Reset curr 
+                    curr = 0;
+                }
+            }
+            ret += curr;
+            return ret;
+        }
+
+        // Driver code
+        static void Main()
+        {
+            string S = "11aa32bbb5";
+            Console.WriteLine(sumOfDigits(S));//op 48
+        }
+    }
+}
