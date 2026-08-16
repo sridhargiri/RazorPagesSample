4633cc7c2db1f4dc578c3f03dee007646a2fa79b
Sridhar Swaminathan <sridhar.swaminathan@puresoftware.com>
2026-08-16 19:21:57 +0530

    Andela

---
+++ ConsoleApp1/ConsoleApp1/GeneralCoding/Andela.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /*
+     using System.Collections.Generic;
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class Tests
+{
+  Subscription plan = new Subscription(1, 1, 5000);
+
+  User[] users = {
+    new User(1, "Employee #1", new DateTime(2019, 1, 1), DateTime.MaxValue, 1),
+    new User(2, "Employee #2", new DateTime(2019, 1, 1), DateTime.MaxValue, 1)
+  };
+
+  [Test]
+  public void WorksWhenNoUsersAreActive() {
+    Assert.That(Challenge.MonthlyCharge("2018-10", plan, users), Is.EqualTo(0));
+  }
+
+  [Test]
+  public void WorksWhenTheActiveUsersAreActiveTheEntireMonth() {
+    int expectedUserCount = 2;
+    Assert.That(
+      Challenge.MonthlyCharge("2020-12", plan, users),
+      Is.EqualTo(expectedUserCount * 5000).Within(1)
+    );
+  }
+}
+     */
+    class Andela
+    {
+    }
+}
